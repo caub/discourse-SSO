6f1fb6c6a4d8aa2b806347658404878f4bac0454
caub <cyril.auburtin@gmail.com>
2017-03-18 09:50:55 +0100

    add logs, for debug (todo winston later maybe)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const crypto = require('crypto');
 const querystring = require('querystring');
 
 
-module.exports = ssoRedirectUrl;
+module.exports = ssoUrl;
 
 
 /*
@@ -13,19 +13,21 @@ module.exports = ssoRedirectUrl;
 
  returns redirect url, can throw if sig mismatch
  */
-function ssoRedirectUrl({sso, sig}, user, ssoSecret = process.env.SSO_SECRET, fields) {
+function ssoUrl({sso, sig}, user, ssoSecret = process.env.SSO_SECRET, fields) {
 
 	const digest = crypto.createHmac('sha256', ssoSecret).update(sso).digest('hex');
 
 	if (digest!==sig) throw new Error('Signature mismatch');
 
 	const {nonce, return_sso_url} =  querystring.parse(Buffer.from(sso, 'base64').toString());
-	
+
 	const return_payload = querystring.stringify(Object.assign(user, {nonce}));
 
 	const return_payload_b64 = Buffer.from(return_payload).toString('base64');
 
 	const digest2 = crypto.createHmac('sha256', ssoSecret).update(return_payload_b64).digest('hex');
+
+	console.info('SSO logged in', user, ssoSecret, return_sso_url);
 
 	return return_sso_url + '?' + querystring.stringify({sso: return_payload_b64, sig: digest2});
 }
